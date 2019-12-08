b4a12c077db20556e55024d60db3783c6374a0c6
Fidentia <43379415+Fidentia@users.noreply.github.com>
2019-12-08 20:17:29 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -125,11 +125,11 @@ gulp.task( 'browser-sync', function() {
 // Run:
 // gulp watch-bs
 // Starts watcher with browser-sync. Browser-sync reloads page automatically on your browser
-gulp.task( 'watch-bs', ['browser-sync', 'watch', 'scripts'], function() { 
+gulp.task( 'watch-bs', ['browser-sync', 'watch', 'scripts'], function() {
 } );
 
-// Run: 
-// gulp scripts. 
+// Run:
+// gulp scripts.
 // Uglifies and concat all JS files into one
 gulp.task( 'scripts', function() {
     var scripts = [
@@ -203,7 +203,7 @@ gulp.task( 'copy-assets', function() {
 
 // UnderStrap SCSS files
     gulp.src( paths.node + 'understrap/sass/**/*.scss' )
-        .pipe( gulp.dest( paths.dev + '/sass/understrap' ) );    
+        .pipe( gulp.dest( paths.dev + '/sass/understrap' ) );
 
     return stream;
 });
