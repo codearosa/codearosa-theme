5c06138dbfd93de878d0dd0d160233e6413def73
Fidentia <43379415+Fidentia@users.noreply.github.com>
2019-12-28 09:51:37 +0100

    Preloader

--- template-particles/preloader.php
+++ template-particles/preloader.php
@@ -1,38 +1,5 @@
 <div class="preloader" aria-hidden="true">
   <div class="falling-div">
-    <i class="fas fa-square primary"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square black"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square grey"></i>
-  </div>
-  <div class="falling-div">
-    <i class="fas fa-square primary"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square black"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square grey"></i>
-  </div>
-  <div class="falling-div">
-    <i class="fas fa-square primary"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square black"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square grey"></i>
-  </div>
-  <div class="falling-div">
-    <i class="fas fa-square primary"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square black"></i>
-  </div>
-  <div class="falling-div">
-  <i class="fas fa-square grey"></i>
+    <?php the_custom_logo(); ?>
   </div>
 </div>
