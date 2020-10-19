dfb108c7f73dc17aa3f0117d9fd2527dd91e6475
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2020-10-19 17:31:12 +0200

    header styles

--- custom/php/functions/0_gutenberg_editor/0004_custom_color-picker.php
+++ custom/php/functions/0_gutenberg_editor/0004_custom_color-picker.php
@@ -1,7 +1,1 @@
-<?php
 
-// -- Disable Custom Colors
-add_theme_support( 'disable-custom-colors' );
-// -- Disable Gradients
-add_theme_support( 'disable-custom-gradients' );
-add_theme_support( 'editor-gradient-presets', array() );
