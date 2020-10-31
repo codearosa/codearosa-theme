c35776bd42bc2cf2c7932136b4d7012dbd521490
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2020-10-31 20:20:30 +0100

    init archiv templates

---
+++ custom/php/templates/content-archive.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+		<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+		<header class="entry-header">
+
+			<?php
+			the_category(
+				sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+				'</a></h3>'
+			);
+			the_title(
+				sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+				'</a></h2>'
+			);
+			?>
+
+		</header><!-- .entry-header -->
+
+
+		<div class="entry-content">
+
+			<?php
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
+
+		</div><!-- .entry-content -->
+
+
+</article><!-- #post-## -->
