9ba6a58634a7c476bf5704a8a5928f613b8b0718
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2020-11-08 17:58:13 +0100

    Revert "Merge branch 'nebenbei-zuckerfrei---20.1' into nebenbei-zuckerfrei-0.0"
    This reverts commit e5b5b04ec810fa8e311374bd2adc005f6a8bb23e, reversing
    changes made to a735d390a0b1c6f78ae41345b5edd44a561a0f61.

--- custom/php/functions/0_gutenberg_editor/0005_custom_theme_color.php
+++ custom/php/functions/0_gutenberg_editor/0005_custom_theme_color.php
@@ -4,22 +4,22 @@ add_theme_support( 'editor-color-palette', array(
 	array(
 		'name'  => __( 'White', 'ea_codearosa_child' ),
 		'slug'  => 'white',
-		'color'	=> '#F6F6F2',
+		'color'	=> '#F2F4F3',
 	),
 	array(
 		'name'  => __( 'Primary', 'ea_codearosa_child' ),
 		'slug'  => 'primary',
-		'color'	=> '#ABB94F',
+		'color'	=> '#C8689A',
 	),
 	array(
 		'name'  => __( 'Secondary', 'ea_codearosa_child' ),
 		'slug'  => 'secondary',
-		'color' => '#F6F6F2',
+		'color' => '#F2F4F3',
 	),
 	array(
 		'name'  => __( 'Success', 'ea_codearosa_child' ),
 		'slug'  => 'success',
-		'color' => '#FFDA2A',
+		'color' => '#2D3142',
 	),
 	array(
 		'name'	=> __( 'Info', 'ea_codearosa_child' ),
@@ -39,7 +39,7 @@ add_theme_support( 'editor-color-palette', array(
 	array(
 		'name'	=> __( 'Dark', 'ea_codearosa_child' ),
 		'slug'	=> 'dark',
-		'color'	=> '#282C11',
+		'color'	=> '#2D3142',
 	),
 ) );
 
