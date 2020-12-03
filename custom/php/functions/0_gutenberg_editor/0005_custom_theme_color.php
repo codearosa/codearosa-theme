376c16edf0dbfbe8d38a5fa607452ecbc1d7e1df
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2020-12-03 19:13:15 +0100

    Update 0005_custom_theme_color.php

--- custom/php/functions/0_gutenberg_editor/0005_custom_theme_color.php
+++ custom/php/functions/0_gutenberg_editor/0005_custom_theme_color.php
@@ -9,37 +9,37 @@ add_theme_support( 'editor-color-palette', array(
 	array(
 		'name'  => __( 'Primary', 'ea_codearosa_child' ),
 		'slug'  => 'primary',
-		'color'	=> '#C8689A',
+		'color'	=> '#780201',
 	),
 	array(
 		'name'  => __( 'Secondary', 'ea_codearosa_child' ),
 		'slug'  => 'secondary',
-		'color' => '#F2F4F3',
+		'color' => '#07436B',
 	),
 	array(
 		'name'  => __( 'Success', 'ea_codearosa_child' ),
 		'slug'  => 'success',
-		'color' => '#2D3142',
+		'color' => '#A1AFB8',
 	),
 	array(
 		'name'	=> __( 'Info', 'ea_codearosa_child' ),
 		'slug'	=> 'info',
-		'color'	=> '#B2B4BA',
+		'color'	=> '#A1AFB8',
 	),
 	array(
 		'name'	=> __( 'Warning', 'ea_codearosa_child' ),
 		'slug'	=> 'warning',
-		'color'	=> '#2D3142',
+		'color'	=> '#780201',
 	),
 	array(
 		'name'	=> __( 'Danger', 'ea_codearosa_child' ),
 		'slug'	=> 'danger',
-		'color'	=> '#2D3142',
+		'color'	=> '#7B3459',
 	),
 	array(
 		'name'	=> __( 'Dark', 'ea_codearosa_child' ),
 		'slug'	=> 'dark',
-		'color'	=> '#2D3142',
+		'color'	=> '#07436B',
 	),
 ) );
 
