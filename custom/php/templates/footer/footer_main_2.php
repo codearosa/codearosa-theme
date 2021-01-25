3d02b4405fb2f746b74cc4bb04a82f3b9c1c1dcb
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-01-25 19:14:43 +0100

    footer blogurl
    show url withour https

--- custom/php/templates/footer/footer_main_2.php
+++ custom/php/templates/footer/footer_main_2.php
@@ -26,7 +26,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 				<div class="site-info text-center">
 
-					&copy; 2009 – <?php echo date('Y'); ?> <a class="text-primary" href="<?php echo site_url(); ?>"> <?php bloginfo( 'url' ); ?></i></a>
+					&copy; <?php echo date('Y'); ?> <a class="text-primary" href="<?php bloginfo( 'url' ); ?>"> <?php $protocols = array('http://', 'http://www.', 'www.'); echo str_replace($protocols, '', get_bloginfo('wpurl')); ?></i></a>
 
 				</div><!-- .site-info -->
 
