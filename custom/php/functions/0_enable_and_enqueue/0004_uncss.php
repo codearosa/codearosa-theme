e72c675850ad9d899a93113f5b15cfae2051257b
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-02-14 11:07:36 +0100

    url parameter to show urls for uncss

---
+++ custom/php/functions/0_enable_and_enqueue/0004_uncss.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+*USE : http://example.dev/?show_sitemap copy and paste it to 
+*/
+
+function show_sitemap() {
+  
+    $the_query = new WP_Query(array('post_type' => 'any', 'posts_per_page' => '-1', 'post_status' => 'publish'));
+    $urls = array();
+    while($the_query->have_posts()) {
+      $the_query->the_post();
+      $urls[] = get_permalink();
+    }
+    die(json_encode($urls));
+  
+}
+add_action('template_redirect', 'show_sitemap');
+
+?>
