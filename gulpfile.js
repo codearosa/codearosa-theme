0def8aba31ce12c799af6698a1da465403682cd7
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-02-14 14:36:35 +0100

    init purgecss with gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,15 @@ gulp.task( 'sass', function() {
     return stream;
 });
 
+//purge unused css
+gulp.task('purgecss', () => {
+    return gulp.src('css/**/*.css')
+        .pipe(purgecss({
+            content: ["http:\/\/localhost\/melaniemueller.design\/styleguide\/", "http:\/\/localhost\/melaniemueller.design\/service\/", "http:\/\/localhost\/melaniemueller.design\/", "http:\/\/localhost\/melaniemueller.design\/lebenslauf\/", "http:\/\/localhost\/melaniemueller.design\/food-app\/", "http:\/\/localhost\/melaniemueller.design\/linkinbio\/", "http:\/\/localhost\/melaniemueller.design\/webdesign-und-frontend-entwicklung-2\/", "http:\/\/localhost\/melaniemueller.design\/portfolio\/", "http:\/\/localhost\/melaniemueller.design\/3-gute-grunde-fur-lokale-schriftarten-in-wordpress\/", "http:\/\/localhost\/melaniemueller.design\/blog\/", "http:\/\/localhost\/melaniemueller.design\/ueber-mich\/", "http:\/\/localhost\/melaniemueller.design\/datenschutzerklaerung-2\/", "http:\/\/localhost\/melaniemueller.design\/impressum\/"]
+        }))
+        .pipe(gulp.dest('css/purge'))
+})
+
 // Run:
 // gulp watch
 // Starts watcher. Watcher runs gulp sass task on changes
@@ -109,7 +118,7 @@ gulp.task( 'cleancss', function() {
     .pipe( rimraf() );
 });
 
-gulp.task( 'styles', gulp.series( 'sass', 'minifycss' ));
+gulp.task( 'styles', gulp.series( 'sass', 'minifycss' , 'purgecss' ));
 
 // Run:
 // gulp browser-sync
@@ -246,10 +255,3 @@ gulp.task( 'dist-product', gulp.series('clean-dist-product', function copyToDist
 gulp.task( 'compile', gulp.series( 'styles', 'scripts', 'dist' ));
 
 
-gulp.task('purgecss', () => {
-    return gulp.src('css/**/*.css')
-        .pipe(purgecss({
-            content: ["http:\/\/localhost\/melaniemueller.design\/styleguide\/", "http:\/\/localhost\/melaniemueller.design\/service\/", "http:\/\/localhost\/melaniemueller.design\/", "http:\/\/localhost\/melaniemueller.design\/lebenslauf\/", "http:\/\/localhost\/melaniemueller.design\/food-app\/", "http:\/\/localhost\/melaniemueller.design\/linkinbio\/", "http:\/\/localhost\/melaniemueller.design\/webdesign-und-frontend-entwicklung-2\/", "http:\/\/localhost\/melaniemueller.design\/portfolio\/", "http:\/\/localhost\/melaniemueller.design\/3-gute-grunde-fur-lokale-schriftarten-in-wordpress\/", "http:\/\/localhost\/melaniemueller.design\/blog\/", "http:\/\/localhost\/melaniemueller.design\/ueber-mich\/", "http:\/\/localhost\/melaniemueller.design\/datenschutzerklaerung-2\/", "http:\/\/localhost\/melaniemueller.design\/impressum\/"]
-        }))
-        .pipe(gulp.dest('css/purge'))
-})
