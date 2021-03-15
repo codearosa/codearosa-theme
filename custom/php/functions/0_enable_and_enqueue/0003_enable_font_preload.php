5295bb64d9c846d456e613bac0bfe234f04c0a85
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-03-15 23:41:59 +0100

    revert commits

--- custom/php/functions/0_enable_and_enqueue/0003_enable_font_preload.php
+++ custom/php/functions/0_enable_and_enqueue/0003_enable_font_preload.php
@@ -8,14 +8,15 @@
 add_action( 'wp_head', function () {
     $fonts_dir_localfont = get_stylesheet_directory_uri() . '/custom/localfont/childtheme_fonts/';
     $fonts_dir_fontawesome = get_stylesheet_directory_uri() . '/custom/localfont/fontawesome/';
-    $fonts_dir_variableFont = get_stylesheet_directory_uri() . '/custom/localfont/variable_font/';
-
 
 	?>
 
-
-<link rel="preload" href="<?php echo $fonts_dir_variableFont . 'Raleway-VariableFont_wght.ttf' ?>" as="font" type="font/ttf" crossorigin="anonymous">
-<link rel="preload" href="<?php echo $fonts_dir_variableFont . 'Raleway-Italic-VariableFont_wght.ttf' ?>" as="font" type="font/ttf" crossorigin="anonymous">
+<link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-700.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-300.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-900.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="<?php echo $fonts_dir_fontawesome . 'fa-brands-400.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="<?php echo $fonts_dir_fontawesome . 'fa-regular-400.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="<?php echo $fonts_dir_fontawesome . 'fa-solid-900.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
 
 
 <?php } );
