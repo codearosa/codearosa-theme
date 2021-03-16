8ccaada2971279f5c97975886da90c1cb62187aa
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-03-16 08:11:08 +0100

    Update 0003_enable_font_preload.php

--- custom/php/functions/0_enable_and_enqueue/0003_enable_font_preload.php
+++ custom/php/functions/0_enable_and_enqueue/0003_enable_font_preload.php
@@ -9,13 +9,13 @@ add_action( 'wp_head', function () {
     $fonts_dir_localfont = get_stylesheet_directory_uri() . '/custom/localfont/childtheme_fonts/';
     $fonts_dir_fontawesome = get_stylesheet_directory_uri() . '/custom/localfont/fontawesome/';
 
+
 	?>
 
 <link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-700.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
 <link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-300.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
 <link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-900.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
-<link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-500.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
-<link rel="preload" href="<?php echo $fonts_dir_localfont . 'raleway-v12-latin-600.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
+
 
 <link rel="preload" href="<?php echo $fonts_dir_fontawesome . 'fa-brands-400.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
 <link rel="preload" href="<?php echo $fonts_dir_fontawesome . 'fa-regular-400.woff2' ?>" as="font" type="font/woff2" crossorigin="anonymous">
