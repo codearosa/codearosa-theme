efa03295c91cd7e42178f579e297e5b751ff8117
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-04-02 19:31:28 +0200

    add single
    add second single

---
+++ custom/php/templates/single/single_2.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part
+ *
+ * @package Codearosa
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<div class="row">
+
+	<header class="entry-header mb-3">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php echo get_the_post_thumbnail( $post->ID, 'full' ); ?>
+
+			<div class="jumbotron-fluid d-flex h-100">
+			
+				<div class="container align-self-center">
+
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+									
+						<p class="lead"><?php understrap_posted_on(); ?></p>
+  				
+				</div>
+  			</div>
+
+	</header><!-- .entry-header -->
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+				
+
+					<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+						
+						<?php get_template_part( 'custom/php/templates/single_content/single-content_2', 'single' ); ?>
+
+						<?php understrap_post_nav(); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+		</div><!-- .row -->
---
+++ custom/php/templates/single_content/single-content_2.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Single post partial template.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+		<div class="entry-meta border border-primary my-3">
+
+			<?php codearosa_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
