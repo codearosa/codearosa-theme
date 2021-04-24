cba44a56974e0db8939e50778c442fc6f831182a
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-04-24 14:24:03 +0200

    home_1
    style the blogpage

---
+++ custom/php/templates/home/home_1.php
@@ -0,0 +1,63 @@
+<?php
+
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php if ( is_front_page() && is_home() ) : ?>
+	<?php get_template_part( 'global-templates/hero' ); ?>
+<?php endif; ?>
+
+<div class="wrapper container home mt-3" id="index-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check and opens the primary div -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+				<div class="card-columns">
+
+				<?php if ( have_posts() ) : ?>
+
+					<?php /* Start the Loop */ ?>
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'custom/php/templates/home/content-home/content-home_1', 'page' );
+						?>
+
+					<?php endwhile; ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+
+				<?php endif; ?>
+			</div>
+			</main><!-- #main -->
+
+			<!-- The pagination component -->
+			<?php understrap_pagination(); ?>
+
+				<!-- Do the right sidebar check -->
+				<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+					
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #index-wrapper -->
+
+<?php get_footer(); ?>
