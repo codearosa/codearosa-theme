64641c6d95b23b004eaac259f8db1d20526a9ab7
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-04-24 16:21:48 +0200

    preloader styles
    add simple js

--- custom/php/templates/preloader/preloader_1.php
+++ custom/php/templates/preloader/preloader_1.php
@@ -1,4 +1,4 @@
-<div class="mmu-preloader-1" aria-hidden="true">
+<div class="mmu-preloader-1 preloader no-js" aria-hidden="true">
   <div class="falling-div">
     <?php the_custom_logo(); ?>
   </div>
--- custom/php/templates/preloader/preloader_2.php
+++ custom/php/templates/preloader/preloader_2.php
@@ -1,4 +1,4 @@
-<div class="mmu-preloader-2" aria-hidden="true">
+<div class="mmu-preloader-2 preloader no-js" aria-hidden="true">
   <div class="falling-div">
     <?php the_custom_logo(); ?>
   </div>
